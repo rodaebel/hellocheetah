7036dbf329627f7c78dad3986450ac47a8bb70b4
Tobias <tobias@LittleGeek.local>
2010-10-30 20:55:29 +0200

    Initial import.

---
+++ README.rst
@@ -0,0 +1,6 @@
+=================================================
+Using The Cheetah Template Engine with GAE/Python
+=================================================
+
+This little *Hello, World!* application demonstrates how to use the Cheetah
+template engine within a Google App Engine application.
---
+++ helloworld.py
@@ -0,0 +1,27 @@
+from Cheetah.Template import Template
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+import os
+
+
+class MainHandler(webapp.RequestHandler):
+  def get(self):
+    path = os.path.join(os.path.dirname(__file__), 'templates/cheetahtest.tmpl')
+    user = users.get_current_user()
+    template_values = {'user': user}
+    tmpl = Template(file=path, searchList=(template_values,))
+    self.response.out.write(tmpl)
+
+
+app = webapp.WSGIApplication([
+  ('/', MainHandler),
+], debug=True)
+
+
+def main():
+  util.run_wsgi_app(app)
+
+
+if __name__ == '__main__':
+  main()
